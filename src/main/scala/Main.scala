24b4b1d30aa9870eed25734586949951e9b0f167
nathan <grimaudathan@gmail.com>
2019-03-11 13:52:28 +0100

    feat: stream

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,23 +1,18 @@
 import org.apache.spark.ml.{Pipeline, PipelineModel}
-import org.apache.spark.ml.evaluation.RegressionEvaluator
-import org.apache.spark.ml.feature.{CountVectorizer, RegexTokenizer, StopWordsRemover, StringIndexer}
+import org.apache.spark.ml.feature.{RegexTokenizer, StopWordsRemover, StringIndexer}
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.{Encoders, SQLContext, SparkSession, Row, DataFrame, Dataset}
+import org.apache.spark.sql.SparkSession
 import org.apache.spark.ml.classification.LogisticRegression
-import org.apache.log4j.Logger
-import org.apache.log4j.Level
+import org.apache.spark._
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
-import org.apache.spark.ml.feature.{HashingTF,IDF}
-
-// https://towardsdatascience.com/multi-class-text-classification-with-pyspark-7d78d022ed35
+import org.apache.spark.ml.feature.{HashingTF, IDF}
+import org.apache.spark.streaming._
 
 case class LabeledText(category: Integer, text: String)
 
 object ML{
-  def train(spark: SparkSession){
-    //We read the data from the file taking into account there's a header.
-    //na.drop() will return rows where all values are non-null.
+  def train(spark: SparkSession): PipelineModel = {
     val df = spark.read
       .format("com.databricks.spark.csv")
       .option("header", true) // Use first line of all files as header
@@ -32,22 +27,21 @@ object ML{
     // split the input to get data on wich we can test our model
     val Array(trainingData, testData) = df.randomSplit(Array(0.7, 0.3),seed= 100)
 
-    val regexTokenizer =  new RegexTokenizer()
+
+    val regexCleaner = new CleanInputTextTransformer()
       .setInputCol("text")
+      .setOutputCol("cleaned_text")
+
+    val regexTokenizer =  new RegexTokenizer()
+      .setInputCol("cleaned_text")
       .setOutputCol("words")
-      .setPattern("\\W") // alternatively .setPattern("\\w+").setGaps(false)
+      .setPattern("\\W") 
 
     val stopwords = Array("is")
     val stopWordsRemover = new StopWordsRemover()
         .setInputCol("words")
         .setOutputCol("filtered")
         .setStopWords(stopwords)
-
-    //val countVectorizer = new CountVectorizer()
-    //  .setInputCol("filtred")
-    // .setOutputCol("features")
-    //.setVocabSize(10000)
-    //.setMinDF(5)
 
     val hashingTF = new HashingTF()
       .setInputCol("filtered")
@@ -72,15 +66,15 @@ object ML{
     val pipeline = new Pipeline()
       .setStages(
         Array(
+          regexCleaner,
           regexTokenizer,
           stopWordsRemover,
           hashingTF,
           idf,
           stringIndexer,
-          //implement tf-idf here
           lr
         )
-        )
+      )
 
     val lrModel = pipeline.fit(trainingData)
     lrModel.write.overwrite.save("lrModel")
@@ -100,24 +94,31 @@ object ML{
     val accuracy = evaluator.evaluate(predictions)
 
     println(s"accuracy : ${accuracy}")
-
+    lrModel
   }
 
-  case class Log(text:String, value: Int)
+  case class Log( text:String, value: Int)
 
-  def predict(spark: SparkSession, log:String){
-    val model = PipelineModel.load("lrModel")
-    val parsedLog = Log(log,4)
-    import spark.implicits._
-    // from implicits :
-    val logs = List(parsedLog)
-    val df = logs.toDF()
-    df.printSchema()
+  def listenStream(conf: SparkConf, session: SparkSession): StreamingContext = {
+    import session.implicits._
+    val ssc = new StreamingContext(session.sparkContext, Seconds(5))
+    val lines = ssc.socketTextStream("localhost", 9999)
+    // we have to train our model before being able to predict
+    val model = ML.train(session)
+    lines.foreachRDD((linesRDD: RDD[String])=>{
+      val transformed = linesRDD.map((line:String)=>{
+        ML.Log(line,4)
+      })
+      val df = transformed.toDF()
+      df.printSchema()
+      val predictions = model.transform(df)
+      predictions
+        .select("text","label","probability","prediction")
+        .show(50)
 
-    val predictions = model.transform(df)
-    predictions
-      .select("text","label","probability","prediction")
-      .show(50)
+     })
+    ssc.start()
+    return ssc
   }
 }
 
@@ -126,16 +127,13 @@ object Main extends App{
   val rootLogger = Logger.getRootLogger()
   rootLogger.setLevel(Level.OFF)
 
-  val spark = SparkSession.builder
-    .appName("log parser")
-    .master("local")
-    .getOrCreate()
+  val conf = new SparkConf()
+    .setMaster("local[2]")
+    .setAppName("log parser")
 
-  ML.train(spark)
-
-  ML.predict(spark, "déc. 29 17:46:34 localhost.localdomain unix_chkpwd[2427]: password check failed for user (root)")
-
-  spark.stop()
-
-
+  val session = SparkSession.builder.config(conf).getOrCreate()
+  val ssc = ML.listenStream(conf, session)
+  
+  ssc.awaitTermination()
+  session.sparkContext.stop()
 }
